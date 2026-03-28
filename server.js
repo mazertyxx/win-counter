26391531346c0f6b7eb024f182e4df4e416f99b6
mazertyxx <mazertyxx@gmail.com>
2026-03-28 16:14:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -132,3 +132,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Serveur running on port " + PORT);
 });
+
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
